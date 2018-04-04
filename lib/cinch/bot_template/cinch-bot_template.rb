b8cb050536d02c7350cdb183b9c7e656709d3b7e
Ken Spencer <me@iotaspencer.me>
2018-04-04 14:39:41 -0400

    add more to requirable file and put the main app inside

--- lib/cinch/bot_template/cinch-bot_template.rb
+++ lib/cinch/bot_template/cinch-bot_template.rb
@@ -1,7 +1,42 @@
+require 'thor'
+require 'cinch/bot_template/main/plugin'
+require 'cinch/bot_template/main/bot'
+require 'cinch/bot_template/main/hello'
 require 'cinch/bot_template/main/cli'
-
+require 'cinch/bot_template/main/spinner'
 module Cinch
   module BotTemplate
+    module CLI
+      class App < Thor
 
+        map %w[--version -v] => :__print_version
+        desc '--version, -v', 'Print the version'
+
+        # Prints version string
+        # @return [NilClass] nil
+        def __print_version
+          puts Cinch::BotTemplate::VERSION
+        end
+
+        desc 'gen [options]', 'Generate a skeleton of a full bot'
+        def gen
+          generator = Cinch::BotTemplate::CLI::Base.new
+          generator.generate
+        end
+
+        desc 'hello', 'Creates a simple hello world bot in one file'
+        def hello
+          generator = Cinch::BotTemplate::CLI::Hello.new
+          generator.generate
+        end
+
+        desc 'plugin [command] [options]', 'Generate a plugin'
+        subcommand('plugin', Cinch::BotTemplate::Plugin)
+
+        desc 'bot [command] [options]', 'Generate a bot file'
+        subcommand('bot', Cinch::BotTemplate::Bot)
+
+      end
+    end
   end
 end
