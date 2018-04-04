73310d13a615c76cf9991408353a1cd1853699b9
Ken Spencer <me@iotaspencer.me>
2018-04-04 14:39:00 -0400

    add more to main bot file maker

--- lib/cinch/bot_template/main/bot.rb
+++ lib/cinch/bot_template/main/bot.rb
@@ -1,9 +1,13 @@
+require  'thor'
 module Cinch
   module BotTemplate
-    class Bot
+
+    class Bot < Thor
+      desc 'gen [options]', 'Generate the executable for a bot'
       # @param [String] filename Name of the plugin
-      def initialize(filename, bot_name,)
+      def gen(filename, bot_name)
         @plugin_name = filename
+        @bot_name = bot_name
       end
     end
   end
