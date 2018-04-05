b16c5815864fbc02e6b77a65c72e1fc2dd2064a5
Ken Spencer <me@iotaspencer.me>
2018-04-05 07:40:18 -0400

    thor files in main

--- lib/cinch/bot_template/main/bot.rb
+++ lib/cinch/bot_template/main/bot.rb
@@ -6,8 +6,8 @@ module Cinch
       desc 'gen [options]', 'Generate the executable for a bot'
       # @param [String] filename Name of the plugin
       def gen(filename, bot_name)
-        @plugin_name = filename
-        @bot_name = bot_name
+        generator = Cinch::BotTemplate::Classes::Bot.new
+        generator.generate
       end
     end
   end
--- lib/cinch/bot_template/main/plugin.rb
+++ lib/cinch/bot_template/main/plugin.rb
@@ -1,11 +1,14 @@
+require 'cinch/bot_template/classes/plugin'
 require 'thor'
+
 module Cinch
   module BotTemplate
     class Plugin < Thor
       desc 'gen [options]', 'Generate a plugin'
       # @param [String] plugin_name Name of the plugin
-      def gen(plugin_name)
-        @plugin_name = plugin_name
+      def gen
+        generator = Cinch::BotTemplate::Classes::Plugin.new
+        generator.generate
       end
     end
   end
