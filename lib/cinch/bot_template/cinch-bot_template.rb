741a5e9dce35371dee79b443c44e44461f86cc5e
Ken Spencer <me@iotaspencer.me>
2018-04-07 09:36:35 -0400

    add more to the main cli class

--- lib/cinch/bot_template/cinch-bot_template.rb
+++ lib/cinch/bot_template/cinch-bot_template.rb
@@ -1,13 +1,19 @@
 require 'thor'
+require 'cinch/bot_template/main/thor'
+require 'cinch/bot_template/main/desc'
 require 'cinch/bot_template/main/plugin'
 require 'cinch/bot_template/main/bot'
 require 'cinch/bot_template/classes/hello'
+require 'cinch/bot_template/main/config'
+
 require 'cinch/bot_template/main/cli'
 require 'cinch/bot_template/main/spinner'
+
 module Cinch
   module BotTemplate
     module CLI
       class App < Thor
+        class_option(:debug, type: :boolean, hide: true)
 
         map %w[--version -v] => :__print_version
         desc '--version, -v', 'Print the version'
@@ -18,13 +24,19 @@ module Cinch
           puts Cinch::BotTemplate::VERSION
         end
 
-        desc 'gen [options]', 'Generate a skeleton of a full bot'
-        def gen
-          generator = Cinch::BotTemplate::CLI::Base.new
+        desc 'gen [DIR] [options]', 'Generate a skeleton of a full bot'
+        method_option 'multi-server', type: :boolean, default: false, aliases: %w(-m), required: false, hide: true
+
+        long_desc Cinch::BotTemplate::Descs::Gen.Gen
+
+        def gen(dir)
+          generator = Cinch::BotTemplate::CLI::Base.new(dir, options: options)
           generator.generate
         end
 
         desc 'hello', 'Creates a simple hello world bot in one file'
+        long_desc Cinch::BotTemplate::Descs::Hello.Gen
+
         def hello
           generator = Cinch::BotTemplate::Classes::Hello.new
           generator.generate
@@ -35,6 +47,9 @@ module Cinch
 
         desc 'bot [command] [options]', 'Generate a bot file'
         subcommand('bot', Cinch::BotTemplate::Bot)
+
+        desc 'config [command] [options]', 'Generate a bot config'
+        subcommand('config', Cinch::BotTemplate::Config)
 
       end
     end
