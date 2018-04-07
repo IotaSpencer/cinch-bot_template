52bc33a6b331c12e6f48990074a79db806f8e03f
Ken Spencer <me@iotaspencer.me>
2018-04-07 09:37:29 -0400

    use autoload to save overhead

---
+++ lib/cinch/bot_template/main/desc.rb
@@ -0,0 +1,10 @@
+module Cinch
+  module BotTemplate
+    module Descs
+      autoload :Bot, 'cinch/bot_template/desc/bot'
+      autoload :Gen, 'cinch/bot_template/desc/gen'
+      autoload :Hello, 'cinch/bot_template/desc/hello'
+      autoload :Plugin, 'cinch/bot_template/desc/plugin'
+    end
+  end
+end
