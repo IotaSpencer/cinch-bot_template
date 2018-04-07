4e39fd2793a1b77262810d1f54aae798b23f5ce1
Ken Spencer <me@iotaspencer.me>
2018-04-07 09:37:00 -0400

    cli class

--- lib/cinch/bot_template/main/cli.rb
+++ lib/cinch/bot_template/main/cli.rb
@@ -1,48 +1,25 @@
 require 'thor'
 require 'highline'
+require 'cinch/bot_template/classes/bot'
+require 'cinch/bot_template/classes/plugin'
+require 'cinch/bot_template/classes/config'
 module Cinch
   module BotTemplate
     module CLI
       class Base
-        def initialize
+        def initialize(directory, options)
           @hl = HighLine.new($stdin, $stderr, 80)
           @opts = Hash.new{ |hash, key| hash[key] = {} }
-
-        end
-
-
-        def get_001_bot_name
-          @hl.say "What's the bot's name"
-          @opts['bot']['name'] = @hl.ask "    > ", String
-        end
-        # @note What the executable file will be named + .rb
-        def get_002_bot_file
-          @hl.say "What should the executable file be named."
-          @hl.say "The generator will add .rb automatically."
-          filename = @hl.ask "    > ", String
-
-          @opts['bot']['file'] = filename.include?('.rb') ? filename : filename + '.rb'
-        end
-        # @note Whether the bot will be multi-server/multi-network
-        # @overload :get_003_bot_multi_server
-        #   This makes the generator use more threads to make a bot instance
-        #   connect to multiple networks.
-        def get_003_bot_multi_server
-          @hl.say "Do you want the bot to be multi-server"
-          @opts['bot']['multi-server'] = @hl.agree "    > ", true
+          @options = options
+          @directory = directory
         end
 
         def generate
-          meths = self.methods.select { |x| x =~ /^get_[0-9]+_.*/ }
-          meths.sort! { |m, n| m.to_s.gsub(/^get_([0-9]+)_.*/, '\1').to_i <=> n.to_s.gsub(/^get_([0-9]+)_.*/, '\1').to_i }
-          meths.each do |m|
-            self.send(m)
-          end
-          @hl.say "Generating... "
-          puts @opts.inspect
-          Cinch::BotTemplate.show_wait_spinner(10) do
+          bot = Cinch::BotTemplate::Classes::Bot.new(directory: @directory, options: @options, all: true)
+          bot.generate
 
-          end
+          config = Cinch::BotTemplate::Classes::Config.new(directory: @directory, options: @options, all: true)
+          config.generate
         end
       end
     end
