66318173cbb17f78a8b52f8b26f14997bb867f83
Ken Spencer <me@iotaspencer.me>
2018-04-11 14:07:46 -0400

    load gen_init to start off the main gen command and more
    * load gen_init
    * remove directory from instance variables
    * add all the parts in for generation

--- lib/cinch/bot_template/main/cli.rb
+++ lib/cinch/bot_template/main/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'highline'
+require 'cinch/bot_template/classes/gen_init'
 require 'cinch/bot_template/classes/bot'
 require 'cinch/bot_template/classes/plugin'
 require 'cinch/bot_template/classes/config'
@@ -7,20 +8,42 @@ module Cinch
   module BotTemplate
     module CLI
       class Base
-        def initialize(directory, shell, options)
-          @hl = HighLine.new($stdin, $stderr, 80)
-          @opts = Hash.new{ |hash, key| hash[key] = {} }
+        def initialize(shell:, options:)
+          @hl      = HighLine.new($stdin, $stderr, 80)
+          @opts    = Hash.new { |hash, key| hash[key] = {} }
           @options = options
-          @directory = directory
-          @shell = shell
+          @shell   = shell
         end
 
         def generate
-          bot = Cinch::BotTemplate::Classes::Bot.new(directory: @directory, options: @options, all: true)
-          bot.generate
+          init = Cinch::BotTemplate::Classes::Init.new(
+              options: @options,
+              shell:   @shell,
+              all:     true
+          )
+          init.generate
+          directory = init.directory
 
-          config = Cinch::BotTemplate::Classes::Config.new(options: @options, shell: @shell, all: true)
-          config.generate
+          config = Cinch::BotTemplate::Classes::Config.new(
+              options: @options,
+              shell:   @shell,
+              all:     true
+          )
+          cfg_path = config.generate(directory)
+
+          bot = Cinch::BotTemplate::Classes::Bot.new(
+              options: @options,
+              shell:   @shell,
+              all:     true
+          )
+          bot.generate(directory: directory, config_file: cfg_path)
+
+          plugins = Cinch::BotTemplate::Classes::Plugin.new(
+              shell: @shell,
+              options: @options,
+              all:     true
+          )
+          plugins.generate(directory: directory)
         end
       end
     end
